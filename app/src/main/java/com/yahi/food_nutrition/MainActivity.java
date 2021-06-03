64e3903c51c3f1fb288836809dae6618a016d183
blackscure <Wekesabuyahi@gmail.com>
2021-06-03 21:36:02 +0300

    initial commits

---
+++ app/src/main/java/com/yahi/food_nutrition/MainActivity.java
@@ -0,0 +1,41 @@
+package com.yahi.food_nutrition;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH = 3300;
+
+    Animation topAnim, bottomAnim;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+       // topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+       // bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        textView = findViewById(R.id.text);
+
+        textView.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
+    }
+}

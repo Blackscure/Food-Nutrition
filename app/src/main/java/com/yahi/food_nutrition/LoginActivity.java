a2bd58ef5bea0a90e45a5a3a3731d4e0fb06d61b
blackscure <Wekesabuyahi@gmail.com>
2021-06-05 00:49:23 +0300

    login func created

--- app/src/main/java/com/yahi/food_nutrition/LoginActivity.java
+++ app/src/main/java/com/yahi/food_nutrition/LoginActivity.java
@@ -1,14 +1,19 @@
 package com.yahi.food_nutrition;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,6 +21,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText loginEmail, loginPwd;
     private Button loginBtn;
     private TextView loginQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
 
 
     @Override
@@ -27,8 +35,8 @@ public class LoginActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.logintoolBar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Login");
-
-
+        mAuth = FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
 
         loginEmail = findViewById(R.id.loginEmail);
         loginPwd = findViewById(R.id.loginPasswd);
@@ -40,6 +48,46 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent( LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPwd.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    loginEmail.setError("email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    loginPwd.setError("password is required");
+                    return;
+                } else {
+                    loader.setMessage("Login in progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+                            } else {
+                                String error = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Registration failed" + error, Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+
+                        }
+                    });
+                }
+
             }
         });
     }

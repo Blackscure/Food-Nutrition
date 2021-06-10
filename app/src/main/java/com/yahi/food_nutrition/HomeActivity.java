9d17c580f076c0634d3a3fe55f93ea3116be2d9f
blackscure <Wekesabuyahi@gmail.com>
2021-06-10 22:45:37 +0300

    data retrived frm databas

--- app/src/main/java/com/yahi/food_nutrition/HomeActivity.java
+++ app/src/main/java/com/yahi/food_nutrition/HomeActivity.java
@@ -4,10 +4,8 @@ import android.app.ProgressDialog;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +13,8 @@ import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -106,31 +106,31 @@ public class HomeActivity extends AppCompatActivity {
                 String id = reference.push().getKey();
                 String date = DateFormat.getDateInstance().format(new Date());
 
-                if (TextUtils.isEmpty(mMeal)){
+                if (TextUtils.isEmpty(mMeal)) {
                     meal.setError("Meal Required");
                     return;
                 }
-                if (TextUtils.isEmpty(mBrand)){
+                if (TextUtils.isEmpty(mBrand)) {
                     brand.setError("Brand required");
                     return;
                 }
-                if (TextUtils.isEmpty(mCalories)){
+                if (TextUtils.isEmpty(mCalories)) {
                     calories.setError("Calories Required");
                     return;
-                }else {
+                } else {
                     loader.setMessage("Adding your data");
                     loader.setCanceledOnTouchOutside(false);
                     loader.show();
 
-                    Model model = new  Model(mMeal,mBrand,mCalories,id,date);
+                    Model model = new Model(mMeal, mBrand, mCalories, id, date);
 
-                   reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    reference.child(id).setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
+                            if (task.isSuccessful()) {
                                 Toast.makeText(HomeActivity.this, "Meal has been inserted", Toast.LENGTH_SHORT).show();
                                 loader.dismiss();
-                            }else {
+                            } else {
                                 String error = task.getException().toString();
                                 Toast.makeText(HomeActivity.this, "Failed" + error, Toast.LENGTH_SHORT).show();
                                 loader.dismiss();
@@ -143,5 +143,61 @@ public class HomeActivity extends AppCompatActivity {
 
         });
         dialog.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Model> options = new FirebaseRecyclerOptions.Builder<Model>()
+                .setQuery(reference,Model.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Model, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Model, MyViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull Model model) {
+                holder.setDate(model.getDate());
+                holder.setMeal(model.getMeal());
+                holder.setBrand(model.getBrand());
+                holder.setCalories(model.getCalories());
+
+            }
+
+            @NonNull
+            @Override
+            public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.retrieved_layout,parent,false);
+                return new MyViewHolder(view);
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        View mview;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mview = itemView;
+        }
+
+        public void setDate(String meal){
+            TextView dateTextView = mview.findViewById(R.id.datetv);
+            dateTextView.setText(meal);
+        }
+
+        public void setMeal(String meal){
+            TextView mealTextView = mview.findViewById(R.id.mealtv);
+            mealTextView.setText(meal);
+        }
+        public void setBrand(String brand){
+            TextView brandTextView = mview.findViewById(R.id.brandtv);
+            brandTextView.setText(brand);
+        }
+        public void setCalories(String calories){
+            TextView caloriesTextView = mview.findViewById(R.id.caloriestv);
+            caloriesTextView.setText(calories);
+        }
     }
 }
